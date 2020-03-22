d375e8cedae24e176da33982dbf0a78329b261e0
Chanthima Pinkaew <chunthima15@gmail.com>
2020-03-22 12:13:02 +0700

    manage code

--- Act1/Act1.ino
+++ Act1/Act1.ino
@@ -2,18 +2,22 @@ void setup(){
     Serial.begin(9600);
     pinMode(13, OUTPUT);
     pinMode(2, INPUT_PULLUP);
-
-/* pinMode(13, OUTPUT);
-pinMode(8, OUTPUT);*/
 }
 void loop(){
     digitalWrite(13, 1);
     delay(100);
     Serial.println(digitalRead(2));
-    /* digitalWrite(13, 0);
+ }
+
+/*void setup() {
+    pinMode(13, OUTPUT);
+    pinMode(8, OUTPUT);
+}
+void loop() {
+    digitalWrite(13, 0);
     digitalWrite(8, 1);
     delay(500);
     digitalWrite(13, 1);
     digitalWrite(8, 0);
-    delay(500); */
-}
+    delay(500);
+}*/
